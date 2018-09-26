d691ee11ce4e68adafcc632bf1e10752a0085d23
adityahenanda <henanda.aditya@gmail.com>
2018-09-26 23:09:37 +0700

    add init

---
+++ Com.Service.TaxCalculation.Lib/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Service.TaxCalculation.Lib
+{
+    class DbContext
+    {
+    }
+}
---
+++ Com.Service.TaxCalculation.Lib/Facade/ProductFacade/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Service.TaxCalculation.Lib.Facade.ProductFacade
+{
+    class ProductViewModel
+    {
+    }
+}
