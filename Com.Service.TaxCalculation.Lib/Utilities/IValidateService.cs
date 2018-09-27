b75c2bffce99f7841beba13ec6c3c7e7ab48d3d7
adityahenanda <henanda.aditya@gmail.com>
2018-09-27 23:45:45 +0700

    Revert "add webapi"
    This reverts commit 1ac66f55917fb8301e7b2523f347f3f2308ac8d5.

---
+++ Com.Service.TaxCalculation.Lib/Utilities/IValidateService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Service.TaxCalculation.Lib.Utilities
+{
+    public interface IValidateService
+    {
+        void Validate(dynamic viewModel);
+    }
+}
---
+++ Com.Service.TaxCalculation.Lib/Utilities/Interfaces/IBaseFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Service.TaxCalculation.Lib.Utilities.Interfaces
+{
+    public interface IBaseFacade<TModel>
+    {
+        Task<int> CreateAsync(TModel model);
+        Task<TModel> ReadByIdAsync(int id);
+    }
+}
